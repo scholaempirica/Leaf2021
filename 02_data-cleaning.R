c04bed127c1c46f3b6bb044e0828f4da68a22513
Aleš Vomáčka <alesvomacka@seznam.cz>
2021-06-21 11:40:05 +0200

    cleaned country of living variable and started descriptive report

--- 02_data-cleaning.R
+++ 02_data-cleaning.R
@@ -3,11 +3,38 @@
 library(tidyverse)
 library(writexl)
 
+leaf <- read_rds("data-input/leaf.rds")
+
 # Data cleaning -----------------------------------------------------------
 
 leaf <- leaf %>%
         mutate(across(.cols = starts_with("S2Q5"),
-                      .fns  = ~fct_relabel(., str_replace,  "^.+ = ", "")))
+                      .fns  = ~fct_relabel(., str_replace,  "^.+ = ", "")), # some variables had labels embedded in values, removed labels
+               across(.cols = c("S2Q3_a", "S5Q1_a"),
+                      .fns  = ~ str_extract(., "[:digit:]")), #some variable included labels combined with values
+               across(.cols = c("S2Q3_a", "S5Q1_a"),
+                      .fns  = ~as.numeric(.)),
+               S4Q5 = fct_explicit_na(S4Q5, "Iné")) # Not-living-in-Slovakia is a valid value, not NA
+
+
+# Collapsing country of living into smaller number of categories
+england     <- c("Uk", "Spojene kralovstvo", "Velka britania", "Spojené kráľovstvo", "Anglicko", "United kingdom", "Veľká británia")
+czechia     <- c("Česká republika", "Česko", "Cr", "Cesko", "Čr", "Česko ", "Cz", "Czechia", "Čr ", "Cz, praha")
+austria     <- c("Rakúsko", "Rakusko")
+switzerland <- c("Svajciarsko", "Švajčiarsko")
+uae         <- c("United arab emirates", "Spojené arabské emiráty")
+usa         <- c("Usa", "Hawaii, usa")
+
+leaf <- leaf %>%
+  mutate(S4Q5_other = str_to_sentence(S4Q5_other),
+         S4Q5_other = case_when(S4Q5_other %in% england     ~ "Velká Britania",
+                                S4Q5_other %in% czechia     ~ "Česká Republika",
+                                S4Q5_other %in% austria     ~ "Rakúsko",
+                                S4Q5_other %in% switzerland ~ "Švajčiarsko",
+                                S4Q5_other %in% uae         ~ "Spojené arabské emiráty",
+                                S4Q5_other %in% usa         ~ "Spojené státy Americké",
+                                TRUE                        ~ S4Q5_other))
+
 
 leaf_xlsx <- leaf
 names(leaf_xlsx) <-  paste(names(leaf_xlsx), attr(leaf_xlsx, "variable.labels"), sep = "_") # Added labels to var names, so that they can
