2ec75d80a1de51406557f61ade100b00a6ab4124
Aleš Vomáčka <alesvomacka@seznam.cz>
2021-06-23 15:19:35 +0200

    Created two composite scales to dataset (dimension 3 and dimension 4)

--- 02_data-cleaning.R
+++ 02_data-cleaning.R
@@ -66,7 +66,77 @@ leaf <- leaf %>%
                                   . == "" ~ NA_character_,
                                   TRUE ~ .)))
 
+# collapsing S2Q3_a scale
 
+leaf <- leaf %>%
+  mutate(S2Q3_coll = case_when(S2Q3_a >= 5 ~ "5-6",
+                                S2Q3_a >= 3 ~ "3-4",
+                                S2Q3_a >= 1 ~ "1-2"),
+         S2Q3_coll = as.factor(S2Q3_coll)) #LEAF wanted to use collapsed scale in the analysis
+
+attr(leaf, which = "variable.labels") <- c(attr(leaf, "variable.labels"), attr(leaf, "variable.labels")[25]) #added the same label as the original scale
+
+var_labs <- attr(leaf, "variable.labels")
+
+# Scoring S1Q1 scale
+
+leaf <- leaf %>%
+  mutate(S1Q1_an = case_when(S1Q1_a == "vôbec nesúhlasím" ~ -1,
+                             S1Q1_a == "skôr nesúhlasím"  ~ -0.5,
+                             S1Q1_a == "skôr súhlasím"    ~  0.5,
+                             S1Q1_a == "určite súhlasím"  ~  1,
+                             S1Q1_a == "neviem posúdiť"   ~  0),
+         S1Q1_bn = case_when(S1Q1_b == "vôbec nesúhlasím" ~  1,
+                             S1Q1_b == "skôr nesúhlasím"  ~  0.5,
+                             S1Q1_b == "skôr súhlasím"    ~ -0.5,
+                             S1Q1_b == "určite súhlasím"  ~ -1,
+                             S1Q1_b == "neviem posúdiť"   ~  0),
+         S1Q1_dn = case_when(S1Q1_d == "vôbec nesúhlasím" ~ -2,
+                             S1Q1_d == "skôr nesúhlasím"  ~ -1,
+                             S1Q1_d == "skôr súhlasím"    ~  1,
+                             S1Q1_d == "určite súhlasím"  ~  2,
+                             S1Q1_d == "neviem posúdiť"   ~  0),
+         across(.cols = c("S1Q1_c", "S1Q1_e", "S1Q1_f"),
+                .fns  = ~case_when(. == "vôbec nesúhlasím" ~  2,
+                                   . == "skôr nesúhlasím"  ~  1,
+                                   . == "skôr súhlasím"    ~ -1,
+                                   . == "určite súhlasím"  ~ -2,
+                                   . == "neviem posúdiť"   ~  0),
+                .names = "{.col}n")) %>%
+  rowwise() %>%
+  mutate(dimension3 = sum(c_across(S1Q1_an:S1Q1_fn))) %>%
+  ungroup() %>%
+  select(-c(S1Q1_an:S1Q1_fn))
+
+attr(leaf, "variable.labels") <- c(var_labs, "Dimenze 3") # rowwise() strips attributes, including variable labels.
+                                                          # Had to readd them and add label add label for dimension3
+
+# Scoring scale S3Q1
+
+var_labs <- attr(leaf, "variable.labels")
+
+labels <- tibble(name  = names(leaf),
+                 label = attr(leaf, "variable.labels")) %>%
+  filter(str_detect(name, "S3Q1"))
+
+leaf <- leaf %>%
+  mutate(across(.cols = c("S3Q1_b", "S3Q1_q"),
+                .fns  = ~case_when(. == "áno" ~ 2,
+                                         TRUE ~ 0),
+                .names = "{.col}n"),
+         across(.cols = c("S3Q1_a", "S3Q1_c", "S3Q1_e", "S3Q1_j", "S3Q1_n", "S3Q1_o"),
+                .fns  = ~case_when(. == "áno" ~ 1,
+                                   TRUE       ~ 0),
+                .names = "{.col}n")) %>%
+  rowwise() %>%
+  mutate(dimension4 = sum(c_across(S3Q1_bn:S3Q1_on))) %>%
+  ungroup() %>%
+  select(-c(S3Q1_bn:S3Q1_on))
+
+attr(leaf, "variable.labels") <- c(var_labs, "Dimenze 4") # Again, rowwise() strips labels
+
+
+# Preparing export to Excel -----------------------------------------------
 
 leaf_xlsx <- leaf
 names(leaf_xlsx) <-  paste(names(leaf_xlsx), attr(leaf_xlsx, "variable.labels"), sep = "_") # Added labels to var names, so that they can
